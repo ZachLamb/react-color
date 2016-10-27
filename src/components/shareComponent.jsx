74c181189d827380171204e2f9ea28b041f88ce0
sperry94 <sepe4375@colorado.edu>
2016-10-27 14:26:45 -0600

    actually fixing null grid and closing modal if grid/user is not found

--- src/components/shareComponent.jsx
+++ src/components/shareComponent.jsx
@@ -69,39 +69,46 @@ export default class ShareComponent extends React.Component {
   }
 
   handleShare(){
-    const usersRef = firebase.database().ref( 'users/' );
+      if(this.props.gridId !== "null")
+      {
+        const usersRef = firebase.database().ref( 'users/' );
 
-    usersRef.orderByChild( 'email' ).equalTo( this.state.userToShareWith ).once( "child_added", userSnapshot => {
-      if( userSnapshot.val() !== null ){
-        const gridRef = firebase.database().ref( 'grids/' + this.props.gridId );
-        const newGridUserEntryRef = gridRef.child( 'users' ).child( userSnapshot.key );
+        usersRef.orderByChild( 'email' ).equalTo( this.state.userToShareWith ).once( "child_added", userSnapshot => {
+          if( userSnapshot.val() !== null ){
+            const gridRef = firebase.database().ref( 'grids/' + this.props.gridId );
+            const newGridUserEntryRef = gridRef.child( 'users' ).child( userSnapshot.key );
 
-        gridRef.once( 'value', gridRefSnapshot => {
-            // only add user to grid if the user is not on the grid
-            if( gridRefSnapshot.child( 'users' ).child( userSnapshot.key ).val() === null ){
-                // update the grid's list of users
-                newGridUserEntryRef.set( true );
+            gridRef.once( 'value', gridRefSnapshot => {
+                // only add user to grid if the user is not on the grid
+                if( gridRefSnapshot.child( 'users' ).child( userSnapshot.key ).val() === null ){
+                    // update the grid's list of users
+                    newGridUserEntryRef.set( true );
+                }
+                else{
+                    console.log( "The specified user already is already listed on this grid" );
+                }
+
+                // only add grid to user if the grid is not on the user
+                if( userSnapshot.child( 'grids' ).child( this.props.gridId ).val() === null ){
+                    // update the user's list of grids
+                    const newUserGridEntryRef = firebase.database().ref( 'users/' + userSnapshot.key + '/grids/' + this.props.gridId );
+                    newUserGridEntryRef.set( gridRefSnapshot.child( 'name' ).val() );
+                }
+                else{
+                    console.log( "The specified grid already is already listed on this user" );
+                }
 
                 // closes modal window & updates user list
                 this.setState({ showModal: false });
                 this.getUserEmails( this.props.gridId );
-            }
-            else{
-                console.log( "The specified user already is already listed on this grid" );
-            }
 
-            // only add grid to user if the grid is not on the user
-            if( userSnapshot.child( 'grids' ).child( this.props.gridId ).val() === null ){
-                // update the user's list of grids
-                const newUserGridEntryRef = firebase.database().ref( 'users/' + userSnapshot.key + '/grids/' + this.props.gridId );
-                newUserGridEntryRef.set( gridRefSnapshot.child( 'name' ).val() );
-            }
-            else{
-                console.log( "The specified grid already is already listed on this user" );
-            }
-        }, error => { console.log(error) });
+            }, error => { console.log(error) });
+          }
+        } );
       }
-    } );
+      else {
+          this.setState({ showModal: false });
+      }
   }
 
   render(){
