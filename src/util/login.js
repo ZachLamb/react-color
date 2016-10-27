08a440b8c4502958258466f510a1a4fd0a4d61a6
Artur Meletyan <artm95@gmail.com>
2016-10-27 00:33:01 -0600

    did some work to the login

--- src/util/login.js
+++ src/util/login.js
@@ -6,22 +6,28 @@ function promptForLogin(uidCallback) {
 	// NEW PAGE
 	firebase.auth().signInWithRedirect(provider);
 	firebase.auth().getRedirectResult().then(function(result) {
-		if (result.credential) {
-			// This gives you a Google Access Token. You can use it to access the Google API.
-			var token = result.credential.accessToken;
-			// ...
-		}
+
 		// The signed-in user info.
 		var user = result.user;
+	    var uid = user.uid;
+	    var user_email = user.email;
+
+	    // check if account already exists, if not add an entry.
+	    var accountCheck = firebase.database().ref('users/' + uid);
+	    accountCheck.once("value", snapshot => {
+	        if (snapshot.val() === null) {
+	            accountCheck.set({
+	                admin: 'false',
+	                email: user_email,
+	                grids: {}
+	            });
+	        }
+	    });
+
+	    // Pass uid to the specified callback.
+	    uidCallback(user.uid);
 	}).catch(function(error) {
-		// Handle Errors here.
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		// The email of the user's account used.
-		var email = error.email;
-		// The firebase.auth.AuthCredential type that was used.
-		var credential = error.credential;
-		// ...
+	    console.log(error);
 	});
 
 	// POP UP
