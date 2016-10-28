2e3536b6c2a38c64237a961650a5591c88fcbcfa
Zach Lamb <zala3440@colorado.edu>
2016-10-28 16:35:36 -0600

    removed login from main app. Hid data from there as well. Also added in displayName for navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 import NewGrid from './newGrid.jsx';
 
+
+import {manageLogin} from '../util/login.js'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class NavBar extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      displayName: null
+    }
+    // this.handleClick = this.handleClick.bind( this );
+  }
+  componentWillReceiveProps(nextProps) {
+      if (this.props.name !== nextProps.name) {
+      this.setState({displayName: nextProps.name});
+      }
+  }
   render(){
-    return(
+    if(this.state.displayName != "null"){
+      return(
+        <nav className="navbar navbar-dark bg-info">
+          <a className="navbar-brand" href="#">React Color</a>
+          <ul className="nav navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Dashboard <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <NewGrid changeGrid={this.props.changeGrid}/>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">{this.props.name}</a>
+            </li>
+          </ul>
+        </nav>
+      );
+    }
+    else{
+      return(
       <nav className="navbar navbar-dark bg-info">
         <a className="navbar-brand" href="#">React Color</a>
         <ul className="nav navbar-nav">
@@ -21,5 +55,10 @@ export default class NavBar extends React.Component {
         </ul>
       </nav>
     );
+    }
   }
+}
+NavBar.propTypes = {
+  changeGrid: React.PropTypes.func,
+  name: React.PropTypes.string
 }
